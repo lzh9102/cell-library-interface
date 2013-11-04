49ce297d7e62689048c1505e3d0bc1c8b12dabf2
Shih Feng Lin <noahleft@gmail.com>
2013-11-05 01:02:05 +0800

    give an example main function parameter

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ int main(int argc, char *argv[])
 {
 	// cells of interest
 	set<string> cells;
-	cells.insert("not");
-	cells.insert("and2");
-	cells.insert("and3");
-	cells.insert("or2");
+	cells.insert("INVM0N");     //inv
+	cells.insert("CKND2M12N");  //nand2
+	cells.insert("ND3M0N");     //nand3
+	cells.insert("NR2M0N");     //nor2
 
 	// load library
 	Library library;
-	library.loadLibrary("test.lib", cells);
+	library.loadLibrary("l90sprvt_typ.lib", cells);
 
 	return 0;
 }
